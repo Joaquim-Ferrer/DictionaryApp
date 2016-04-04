6d10ac128f2a31a4b0855d47f45a328d43f62d95
Joaquim-Ferrer <jvrbferrer@gmail.com>
2016-04-04 17:27:37 +0100

    Gets the AVL function declarations in the header file and corrects the multiple def guard

--- header_avl.h
+++ header_avl.h
@@ -1,5 +1,7 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef HEADER_AVL
+#define HEADER_AVL
+
+#include <string>
 
 struct AVL_Node {
     Node *parent; 
@@ -12,5 +14,50 @@ struct AVL_Node {
 
     int height;
 };
+
+/*
+**Inserts a word in an AVL tree. In case the tree gets unbalanced, it gets balanced
+
+*/
+int insert_word(AVL_Node *&root, string word, string translation);
+
+/*
+**Searches a node in the subtree with root root with the word word 
+*/
+AVL_Node *search_node(AVL_Node *root, string word);
+
+/*
+**Creates a new node with it's default values, word word, translation translation
+**and parent parent
+*/
+AVL_Node *new_node(string word, string translation, AVL_Node *parent);
+
+/*
+**Rotates the subtree with root node to the right
+*/
+void rotate_right(AVL_Node *node);
+
+/*
+**Rotates the subtree with root node to the left
+*/
+void rotate_left(AVL_Node *node);
+
+/*
+**Updates the height of a node considering the height of its children.
+**Is only correct if it's children heights are correct or are NULL
+*/
+void update_height(AVL_Node *node);
+
+/*
+**Gets the balance factor of a node. It will be positive if its right subtree
+**is deeper than its left subtree. 
+**The possible return values are between -2 and 2.
+*/
+int get_balance(AVL_Node *node);
+
+/*
+**Returns the biggest integer, a or b
+*/
+int max(int a, int b);
 
 #endif
