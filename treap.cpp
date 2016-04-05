89346ab718d1fd89f4ef95e3ad65bbd11cd453f7
Joaquim-Ferrer <jvrbferrer@gmail.com>
2016-04-05 13:31:46 +0100

    Adds add_node function

--- treap.cpp
+++ treap.cpp
@@ -10,14 +10,47 @@ uniform_real_distribution<float> pri_dist (0.0,1.0);
 **Inserts a node in the binary tree whose root is root. The insertion is made
 **to create a binary search tree in which the parent is always alphabetically
 **bigger than ots left son and smaller than the right son. In addition, a son is always
-**less searched than a parent.
+**less searched than a parent. 
 */ 
 int treap::insert_word(Node *&root, string word, string translation) {
     if(root == NULL) {
         root = new_node(word, translation, NULL);
         return 1;
     }
+
+    Node *aux = root;
+
+    while(1) {
+        int comparison = word.compare(aux->word);
+        if(comparison < 0) {
+            if(aux->son_left == NULL) {
+                //Insert new node to the left of the subtree
+                Node *node = new_node(word, translation, aux);
+                aux->son_left = node;
+                break;
+            }
+            aux = aux->son_left;
+        }
+        else if(comparison > 0) {
+            if(aux->son_right == NULL) {
+                //Insert new node to the riht of the subtree
+                Node *node = new_node(word, translation, aux);
+                aux->son_right = node;
+                break;
+            }
+            aux = aux->son_right;
+        }
+        else {
+            //There's a node wih that word already
+            return 0;
+        }
+    }
+    //The new node goes up the tree while its parent ha n_searches 1 and a 
+    //rand_priority lower than his
+    splay(node);
+    return 1;
 }
+
 
 Node *new_node(string word, string translation, Node *parent) {
     Node *new_node = new Node;
@@ -30,5 +63,5 @@ Node *new_node(string word, string translation, Node *parent) {
     new_node->son_right = NULL;
 
     new_node->rand_priority = pri_dist(gen);
-    new_node->n_searches = 0;
+    new_node->n_searches = 1;
 }
