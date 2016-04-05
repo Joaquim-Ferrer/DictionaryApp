d640562618acb69d4903dbf06d0919956d324c2f
Joaquim-Ferrer <jvrbferrer@gmail.com>
2016-04-05 12:40:59 +0100

    Changes name to the header file of treap.cpp and corrects some of its content

---
+++ header_treap.h
@@ -0,0 +1,23 @@
+#ifndef HEADER_TREAP
+#define HEADER_TREAP
+
+using namespace std;
+
+namespace treap {
+    struct Node {
+        Node *parent;
+        Node *son_left;
+        Node *son_right;
+
+        string word;
+        string translation;
+        bool tagged;
+
+        float rand_priority;
+        long int n_searches;
+    };
+
+
+}
+
+#endif
